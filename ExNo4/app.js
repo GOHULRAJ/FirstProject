ead7bd3433d3091739091143f66e48e7c1d6d417
GOHULRAJ <gohulrajs71@gmail.com>
2023-06-05 22:16:18 +0530

    some changes to be done

--- ExNo4/app.js
+++ ExNo4/app.js
@@ -1,10 +1,17 @@
-var express =require ('express')
+var express=require('express')
 var hbs=require('hbs')
 var app=express();
-
+//const path=require(path);
+//app.use('/static',express.static(path.join(__dirname,'public')));
+ 
 app.set('view engine','hbs');
 app.set('views','views');
+
+//app.get('/',(req,res)=>{
+//    res.render('file')
+//})
+
 app.get('/',(req,res)=>{
     res.render('file')
 })
-app.listen(3000)
+app.listen(3020)
